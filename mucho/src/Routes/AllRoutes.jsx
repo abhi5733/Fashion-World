655687e2c374d5ac8e361232c9fc206a9900f2be
abhi5733 <abhijeetkale5733@gmail.com>
2022-11-10 19:01:40 +0530

    added new file

--- mucho/src/Routes/AllRoutes.jsx
+++ mucho/src/Routes/AllRoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route,Routes } from 'react-router-dom'
 import Men from '../Components/Men'
+import Signup from '../Components/Signup'
 import Women from '../Components/Women'
 const AllRoutes = () => {
   return (
@@ -8,6 +9,7 @@ const AllRoutes = () => {
       <Routes>
         <Route path="/" element={<Women/>} />
         <Route path="/Men" element={<Men/>} />
+        <Route path="/Login" element={<Signup/>}  />
       </Routes>
     </div>
   )
